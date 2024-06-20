14dfde2276511b658ef6bfd5c5560c6469410f96
zicojr <fegyisaac160@gmail.com>
2024-06-21 00:04:27 +0100

    deleted dist

--- app/(user)/cart/dist/page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Cart_1 = require("@/components/Cart");
-var CartPage = function () {
-    return (React.createElement("div", null,
-        React.createElement(Cart_1["default"], null)));
-};
-exports["default"] = CartPage;
--- components/dist/Container.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Container = function (_a) {
-    var children = _a.children, className = _a.className;
-    return (React.createElement("div", { className: "w-[900px] max-w-screen-xl mx-auto py-5 px-4 lg:px-0 " + className }, children));
-};
-exports["default"] = Container;
